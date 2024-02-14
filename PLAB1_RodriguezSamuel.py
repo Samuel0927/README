acc8911bf18bfacfdf5a8747996ba0468b0a083a
Samuel0927 <160054005+Samuel0927@users.noreply.github.com>
2024-02-14 12:45:57 -0500

    Add files via upload

---
+++ PLAB1_RodriguezSamuel.py
@@ -0,0 +1,10 @@
+ # Samuel Rodriguez
+ # 02/13/2024
+
+ # P1LAB1
+
+ # This assignment is using input and output
+
+f_name = input("Enter your first name: ")
+print("Hello", f_name)
+print("Welcome to zyBooks!")
